c0b4ba6bf8ab8cbc2670e9d22bd8871d822738a8
Wesley F. Young <willamwei@outlook.com>
2024-06-26 03:12:22 +0800

    Initialize using CLI

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,7 @@
 import { StdhubPluginApi } from 'stdhub-plugin-api';
 import { stdApiDemo } from "./demo";
 
-export const pluginName = 'stdhub-plugin-template';
+export const pluginName = 'demo';
 export const api = new StdhubPluginApi(pluginName);
 
 stdApiDemo();

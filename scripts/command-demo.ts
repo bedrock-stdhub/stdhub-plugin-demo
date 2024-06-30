a80c79b7e5407c2ee1d651e8b26ab2e9b05aa69a
Wesley F. Young <willamwei@outlook.com>
2024-06-30 10:47:03 +0800

    chore: squash each pattern segment into a single line

--- scripts/command-demo.ts
+++ scripts/command-demo.ts
@@ -8,14 +8,8 @@ export function commandDemo() {
     [
       'push',
       [ 'up', 'down' ],
-      {
-        type: 'integer',
-        displayName: 'blocks'
-      },
-      {
-        type: 'boolean',
-        displayName: 'break-original'
-      }
+      { type: 'integer', displayName: 'blocks' },
+      { type: 'boolean', displayName: 'break-original' }
     ] as const,
     (player, [ , direction, blocks, breakOriginal ]) => {
       player.sendMessage(`Trying to push ${direction}wards ${blocks} block(s) and ${breakOriginal ? 'to' : 'not to'} break the original blocks.`);
@@ -26,14 +20,8 @@ export function commandDemo() {
     [
       'pull',
       [ 'up', 'down' ],
-      {
-        type: 'integer',
-        displayName: 'blocks',
-      },
-      {
-        type: 'string',
-        displayName: 'message',
-      }
+      { type: 'integer', displayName: 'blocks', },
+      { type: 'string', displayName: 'message', }
     ] as const,
     (player, [ , direction, blocks, msg ]) => {
       player.sendMessage(`Trying to pull ${direction}wards ${blocks} block(s) and emit message ${msg}`);
